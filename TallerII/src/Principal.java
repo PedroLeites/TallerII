68f621ea02238a026ac4866f07992f7b57427057
Pedro Leites <leitespsinsajo@gmail.com>
2025-05-12 18:31:35 -0300

    Se modifica la clase Principal
    - Se cambia la forma de instanciar la vista y el controlador en Principal
    - Se cambia la forma de recorrer la colección y agregar las filas a la tabla en PrincipalControlador

--- TallerII/src/Principal.java
+++ TallerII/src/Principal.java
@@ -4,10 +4,17 @@ import javax.swing.SwingUtilities;
 
 public class Principal {
 	public static void main(String[] args) {
+		
+		PrincipalVista ventana = new PrincipalVista();
+		PrincipalControlador controladorVentana = new PrincipalControlador(ventana);
+		ventana.setVisible(true);
+		
+		/*
 		SwingUtilities.invokeLater(() -> {
             PrincipalVista view = new PrincipalVista();
             new PrincipalControlador(view);
             view.setVisible(true);
         });
+        */
 	}
 }
--- TallerII/src/controlador/PrincipalControlador.java
+++ TallerII/src/controlador/PrincipalControlador.java
@@ -1,11 +1,13 @@
 package controlador;
 
+import modelo.Estudiante;
 import modelo.ColeccionEstudiantes;
 import persistencia.ImportadorExcel;
 import vista.PrincipalVista;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+import java.util.Iterator;
 
 public class PrincipalControlador implements ActionListener{
 	private PrincipalVista vista;
@@ -22,9 +24,18 @@ public class PrincipalControlador implements ActionListener{
 		 vista.limpiarTabla();
 		 try {
 			 ColeccionEstudiantes colec = importador.importarDeExcel("morosos.xlsx");
-			 colec.obtenerTodos().forEach(est -> 
-			 vista.agregarFila(new Object[]{est.getCI(), est.getNombre(), est.getEmail(), est.getLibro(), est.getFechaVencimiento()})
-	            );
+			 Iterator<Estudiante> it = colec.obtenerTodos().iterator();
+			 while (it.hasNext()) {
+			     Estudiante est = it.next();
+			     vista.agregarFila(new Object[]{
+			         est.getCI(),
+			         est.getNombre(),
+			         est.getEmail(),
+			         est.getLibro(),
+			         est.getFechaVencimiento()
+			     });
+			 }
+			 //colec.obtenerTodos().forEach(est -> vista.agregarFila(new Object[]{est.getCI(), est.getNombre(), est.getEmail(), est.getLibro(), est.getFechaVencimiento()}));
 	        } catch (Exception ex) {
 	            JOptionPane.showMessageDialog(vista, "Error al leer Excel: " + ex.getMessage());
 	        }
