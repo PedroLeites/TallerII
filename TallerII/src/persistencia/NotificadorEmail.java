0ad391d64d5725b54cad1fb3050a6ed037f81146
PedroLeites <leitespsinsajo@gmail.com>
2025-05-13 23:35:05 -0300

    Se agrega la clase NotificadorEmail

--- TallerII/src/persistencia/NotificadorEmail.java
+++ TallerII/src/persistencia/NotificadorEmail.java
@@ -1,5 +1,38 @@
 package persistencia;
 
-public class NotificadorEmail {
+import modelo.Estudiante;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
 
+public class NotificadorEmail {
+	private final String remitente = "taller2inet2025@gmail.com";
+    private final String contraseña = "taller2inet2025PFJM";
+    
+    public void enviarMail(Estudiante est) {
+    	Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(remitente, contraseña);
+            }
+        });
+
+        try {
+            Message mensaje = new MimeMessage(session);
+            mensaje.setFrom(new InternetAddress(remitente));
+            mensaje.setRecipient(Message.RecipientType.TO, new InternetAddress(est.getEmail()));
+            mensaje.setSubject("Aviso de préstamo vencido");
+            mensaje.setText("Estimado/a " + est.getNombre() + ", su préstamo del libro '" + est.getLibro() +
+                "' venció el " + est.getFechaVencimiento() + ".\nPor favor regularice su situación.");
+            Transport.send(mensaje);
+        } catch (Exception e) {
+            System.out.println("Error al enviar correo: " + e.getMessage());
+        }
+    }
+    
 }
