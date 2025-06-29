611f5a023a8fce6506c4dac5b33b02a75179c18f
PedroLeites <leitespsinsajo@gmail.com>
2025-06-29 18:22:59 -0300

    Actualización clase Pestamo

--- TallerII/src/modelo/Prestamo.java
+++ TallerII/src/modelo/Prestamo.java
@@ -1,20 +1,22 @@
 package modelo;
 
 public class Prestamo {
-    private String fechaPrestamo; // aff_pret_date
-    private String fechaDevolucionPrevista; // aff_pret_retour
-    private int diasRetraso; // retard
-    private Estudiante estudiante; // Relación con Estudiante
-    private Libro libro; // Relación con Libro
+    private String fechaPrestamo; //aff_pret_date
+    private String fechaDevolucionPrevista; //aff_pret_retour
+    private int diasRetraso; //calcular con los atributos anteriores
+    private int idLibro; //expl_cb
+    private String tituloLibro; //tit
 
-    public Prestamo() {}
+    public Prestamo() {
+    	
+    }
 
-    public Prestamo(String fechaPrestamo, String fechaDevolucionPrevista, int diasRetraso, Estudiante estudiante, Libro libro) {
+    public Prestamo(String fechaPrestamo, String fechaDevolucion, int diasRetraso, int idLibro, String tituloLibro) {
         this.fechaPrestamo = fechaPrestamo;
-        this.fechaDevolucionPrevista = fechaDevolucionPrevista;
+        this.fechaDevolucionPrevista = fechaDevolucion;
         this.diasRetraso = diasRetraso;
-        this.estudiante = estudiante;
-        this.libro = libro;
+        this.idLibro = idLibro;
+        this.tituloLibro = tituloLibro;
     }
 
     // Getters y Setters
@@ -41,26 +43,28 @@ public class Prestamo {
     public void setDiasRetraso(int diasRetraso) {
         this.diasRetraso = diasRetraso;
     }
+    
+    public int getIdLibro() {
+        return idLibro;
+    }
+    
+    public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
 
-    public Estudiante getEstudiante() {
-        return estudiante;
+	public String getTituloLibro() {
+        return tituloLibro;
     }
 
-    public void setEstudiante(Estudiante estudiante) {
-        this.estudiante = estudiante;
-    }
+	public void setTituloLibro(String tituloLibro) {
+		this.tituloLibro = tituloLibro;
+	}
 
-    public Libro getLibro() {
-        return libro;
-    }
-
-    public void setLibro(Libro libro) {
-        this.libro = libro;
-    }
-
-    @Override
-    public String toString() {
-        return "Prestamo [fechaPrestamo=" + fechaPrestamo + ", fechaDevolucionPrevista=" + fechaDevolucionPrevista +
-               ", diasRetraso=" + diasRetraso + ", estudiante=" + estudiante.getNombre() + ", libro=" + libro.getTitulo() + "]";
-    }
+	@Override
+	public String toString() {
+		return "Prestamo [fechaPrestamo=" + fechaPrestamo + ", fechaDevolucionPrevista=" + fechaDevolucionPrevista
+				+ ", diasRetraso=" + diasRetraso + ", idLibro=" + idLibro + ", tituloLibro=" + tituloLibro + "]";
+	}
+	
+	
 }
