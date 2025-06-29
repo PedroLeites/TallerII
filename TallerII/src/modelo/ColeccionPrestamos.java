63c9c619deff5c31e4c82669ded2f3d1e4a3ebd0
PedroLeites <leitespsinsajo@gmail.com>
2025-06-29 18:26:23 -0300

    Update ColeccionPrestamos.java

--- TallerII/src/modelo/ColeccionPrestamos.java
+++ TallerII/src/modelo/ColeccionPrestamos.java
@@ -19,18 +19,7 @@ public class ColeccionPrestamos {
     public void eliminar(Prestamo p) {
         prestamos.remove(p);
     }
-	
-    // Busca todos los préstamos de un estudiante por su CI
-    public ArrayList<Prestamo> obtenerPorCI(int ci) {
-        ArrayList<Prestamo> resultado = new ArrayList<>();
-        for (int i = 0; i < prestamos.size(); i++) {
-            if (prestamos.get(i).getEstudiante().getCI() == ci) {
-                resultado.add(prestamos.get(i));
-            }
-        }
-        return resultado;
-    }	
-    
+		
     // Devuelve todos los préstamos
     public ArrayList<Prestamo> obtenerTodos() {
         return prestamos;
@@ -39,17 +28,6 @@ public class ColeccionPrestamos {
     // Devuelve la cantidad total de préstamos
     public int largo() {
         return prestamos.size();
-    }
-    
-    // Verifica si existe al menos un préstamo para el estudiante con CI dado
-    public boolean existePrestamoParaEstudiante(int ci) {
-        boolean existe = false; 
-    	for (int i = 0; i < prestamos.size(); i++) {
-            if (prestamos.get(i).getEstudiante().getCI() == ci) {
-                existe = true;
-            }
-        }
-        return existe;
     }
     
     // Verifica si la colección de préstamos está vacía
